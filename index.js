e17b79f5bc8c3426a5f0f4fa993b16ae9c288ce2
Kate <katy8kirichenko@gmail.com>
2021-01-26 12:14:35 +0300

    kakoeto govno

--- index.js
+++ index.js
@@ -1,24 +1,67 @@
 const form = document.querySelector("form");
-const table = document.querySelector("table");
+const table = document.querySelector("tbody");
 const addBtn = document.querySelector("#add");
+const data = [];
+
+const delBtn = document.querySelector("#del");
 
 function addWorker() {
-  const workerName = document.querySelector("#name");
-  const workerBirth = document.querySelector("#birth");
-  const workerDate = document.querySelector("#workDate");
-  const workerSalary = document.querySelector("#salary");
-  table.innerHTML += `<tr>
+  table.innerHTML=""
+  data.forEach((item) => {
+    table.innerHTML += `<tr>
     <td><input type="checkbox" /></td>
-    <td>${workerName.value}</td>
-    <td>${workerBirth.value}</td>
-    <td>${workerDate.value}</td>
-    <td>${workerSalary.value}</td>
+    <td>${item.name}</td>
+    <td>${item.birth}</td>
+    <td>${item.date}</td>
+    <td>${item.salary}</td>
   </tr>`;
+  });
 }
+
+function getSum() {
+  let sum = document.querySelector("#sum");
+  sum.innerHTML = `${data.length}`;
+}
+
+// function getSalary() {
+//   let bill = document.querySelector("bill");
+//   bill.innerHTML = `${data.reduce(salary)}`;
+// }
 
 addBtn.addEventListener("click", (event) => {
   event.preventDefault();
 
+  const workerName = document.querySelector("#name");
+  const workerBirth = document.querySelector("#birth");
+  const workerDate = document.querySelector("#workDate");
+  const workerSalary = document.querySelector("#salary");
+
+  data.push({
+    name: workerName.value,
+    birth: workerBirth.value,
+    date: workerDate.value,
+    salary: workerSalary.value,
+  });
+
   addWorker();
+  getSum();
+  getSalary();
+
   form.reset();
+});
+
+function delWorker() {
+  let element = document.querySelector("input[type=checkbox]:checked");
+
+  data.slice(element, 1)
+
+  addWorker()
+  getSum()
+
+}
+
+delBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  delWorker()
 });
